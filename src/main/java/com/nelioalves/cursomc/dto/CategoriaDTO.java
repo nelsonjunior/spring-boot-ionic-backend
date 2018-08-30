12e6f77bcdf60ce88cc0e04241746ad2ff29b315
Nelson Rodrigues <nrsjnet@gmail.com>
2018-08-30 20:11:53 -0300

    Listando todas categorias com DTO

---
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,72 @@
+package com.nelioalves.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.nelioalves.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+
+	private String nome;
+
+
+	public CategoriaDTO() {
+	}
+	
+	public CategoriaDTO(Categoria categoria) {
+		super();
+		this.id = categoria.getId();
+		this.nome = categoria.getNome();
+	}
+
+	public CategoriaDTO(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoriaDTO other = (CategoriaDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
