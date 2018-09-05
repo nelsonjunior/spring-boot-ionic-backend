e3000287b0f01481a8fc7c5f30820e041bf01200
Nelson Rodrigues <nrsjnet@gmail.com>
2018-09-04 21:51:07 -0300

    Ajustando e testando a deleção de cliente

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
@@ -35,7 +36,7 @@ public class Cliente implements Serializable {
 	@Column(updatable=false)
 	private Integer tipoCliente;
 
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade=CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
